eee0a9b9d427d6bc562db3e8bc8f6832b17f57c6
treasure-sky <ych0486666@gmail.com>
2025-05-06 20:03:26 +0900

    init: 프로젝트 생성

---
+++ src/main/java/edu/kangwon/university/taxicarpool/chattingpractice/ChattingPracticeApplication.java
@@ -0,0 +1,13 @@
+package edu.kangwon.university.taxicarpool.chattingpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChattingPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/kangwon/university/taxicarpool/chattingpractice/ChattingPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kangwon.university.taxicarpool.chattingpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
